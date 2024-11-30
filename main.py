fc3a58ca97804c2ba441cf7b156d816168c1e691
Asa Daboh <160549474+AsaDabola@users.noreply.github.com>
2024-11-30 18:02:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
-#  The start of the program
-#  Where input is processed
-#  All the parts of the code are in one file, decresing scalability, but increasing robustness for this project, since it is relatively small
-#  Read arguements from the terminal on Mac and the Command line on Windows 
 
-# import sys  for terminal use?
+import os
+import argparse
 from parse import parser
 from minimized import minimizer
 from display import show_pla 
 
 def main():
   # Used to parse the input, minimze logic, write the output 
-  in_file = "test_case/input.pla"
-  out_file = "output.pla"
 
-  #parse the input 
-  data = parse(in_file)
+  # Collect the input and ouput count from the testcase file 
+  in_file = args.in_file
+  out_file = args.out_file
+
+  # Check if the file actually exists 
+  if not os.path.exists(in_file):
+    print(f"Error: The input file {in_file} does not exist :( ")
+    return
+
+  #parse the inputs 
+  data = parser(in_file)
   print(f"Parsed Data: {data}")
 
-  # get the inputs from the file test cases
+  # Get the inputs from the file test cases
   inputs = data["inputs"]
   outputs = data["outputs"]
 
-  #Minimize logic with QM
+  # Minimize logic with QM
   minimized_logic = minimizer(data)
-  print(f"Minimized logic using Quine-McCluskey: {minimizer}")
+  print(f"Minimized logic using Quine-McCluskey: {minimized_logic}")
 
-  # write the now minimized logic in PLA format to the output file 
+  # Write the now minimized logic in PLA format to the output file 
   show_pla(out_file, minimized_logic, inputs, outputs)
   print(f"Output written to : {out_file}")
 
-  if __name__ = "__main__":
+if __name__ == "__main__":
     main()
