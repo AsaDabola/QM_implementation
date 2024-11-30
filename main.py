ce9666fdf16c70047fb01908537a0760f7310ef5
Asa Daboh <160549474+AsaDabola@users.noreply.github.com>
2024-11-30 17:22:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,27 @@
 
 
 import sys
-from parser import parse
-from minimizer import minimized
-from utils import show_pla 
+from parse import parser
+from minimized import minimizer
+from display import show_pla 
 
 def main()
   # Used to parse the input, minimze logic, write the output 
 
-inputFile = "test_case/input.pla"
-outputFile = "output.pla"
+in_file = "test_case/input.pla"
+out_file = "output.pla"
 
 #parse the input 
-data = parse(inputFile)
+data = parse(in_file)
 print(f"Parsed Data: {data}")
 
 #Minimize logic with QM
-minimize_logic = minimized(data)
-print(f"Minimized logic using Quine-McCluskey: {minimized}")
+minimize_logic = minimizer(data)
+print(f"Minimized logic using Quine-McCluskey: {minimizer}")
 
 # write the now minimized logic in PLA format to the output file 
-show(outputFile, minimize_logic)
-print(f"Output written to {outputFile}")
+show(out_file, minimize_logic)
+print(f"Output written to {out_file}")
 
 if __name__ = "__main__"
   main()
