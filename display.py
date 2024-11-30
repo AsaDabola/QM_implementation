3eed4673c97e3425c9e5f29ab3147c7e6fed1de6
Asa Daboh <160549474+AsaDabola@users.noreply.github.com>
2024-11-30 18:26:09 -0500

    Update display.py

--- display.py
+++ display.py
@@ -1,15 +1,16 @@
-# Final output of the function
+# Final PLA output
 
-# def show_pla(output_path, minimized_logic): 
-def show_pla(output_path, minimized_logic, num_inputs, num_ouputs)
+def show_pla(output_path, minimized_logic, input_count, ouput_count)
+  
   # Simple code to display the PLA file 
   with open(output_path, "w") as file: 
-    file.write(f".i {num_inputs}\n") 
-    file.write(f".o {num_ouputs}\n")
+    file.write(f".i {input_count}\n") 
+    file.write(f".o {output_count}\n")
+    
     for term in minimized_logic
       file.write(f"{term} 1\n")
-    file.write(".e\n") # PLA file ending
-    # num_inputs = int(input().strip())
-    # num_outputs = int(input().strip())
-
+    # PLA file end
+    file.write(".e\n") 
+    
+   
   
