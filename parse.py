9acbfb40291225570b845b730cc22c0da62591af
Asa Daboh <160549474+AsaDabola@users.noreply.github.com>
2024-11-30 17:28:03 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,23 +1,32 @@
 
 # We need to parse the input so the program can understand and use the input 
-def parse(file_path): 
+def parser(file_path): 
 
   # An array for the minterms binary numbers 
     minterms = []
   # An array for the minterms dont care terms that are usually marked by a "-" marker
     dont_cares = []
+    inputs = 0 # variable for input 
+    outputs = 0 # variable for output 
 
 # We open the file path to the test case 
  with open(file_path, "r") as file: 
    for line in file: 
+       line = line.strip()
+     if line.startswith(".i"): # get the number of inputs from the file
+       inputs = int(line.split()[1])
+     elif line.startswith(".o") # get the number of oututs from the file
+       outputs = int(line.split()[1])
      # this is to skip any comments potentially made in the file and metadata lines 
-     if line.startswith(".") or line.strip() == "": 
+     elif line.startswith(".") or line.strip() == "": 
        continue 
-    # We must parse the input-output pairs 
-    parts = line.srip().split()
-    if parts[1] == "1":
-      minterms.append(parts[0])
-    elif parts[1] == "-": 
-      dont_care.append(parts[0])
+     else: 
+        # parses the input and output from the file 
+        parts = line.srip().split()
+        if parts[1] == "1":
+          minterms.append(parts[0])
+        elif parts[1] == "-": 
+          dont_care.append(parts[0])
+
 
 return {"MIN terms": minterms, "DC terms": dont_cares}
